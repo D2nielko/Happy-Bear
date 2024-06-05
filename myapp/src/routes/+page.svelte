c9e9e5afd8d5af2b05d23e88f893a8af8c763d77
Daniel Ko <dannieko02@gmail.com>
2024-06-05 13:41:19 +0000

    added svelte chatbox example

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Chat from '../lib/Scheduler.svelte';
+	import BouncingDots from '../lib/BouncingDots.svelte'  
+</script>
+
+<main>
+	<Chat />
+</main> 
+
+<style>
+	:global(body) {
+		background: #F5F5DC;
+	}
+	
+	main {
+		width: 500px;
+		margin: 50px auto;
+	} 
+</style>
